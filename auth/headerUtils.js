f49fc5c940c3a3f688ea07f25c5e9f57ee9549ad
Niharika Narain <niharikanarain@gmail.com>
2021-08-26 08:58:40 -0500

    check for token before validating

--- auth/headerUtils.js
+++ auth/headerUtils.js
@@ -4,11 +4,11 @@ const getBearerToken = (bearerHeader = '') => bearerHeader.split(' ')[1];
 const validTokens = readFile('./auth/tokens.json');
 
 const isAdmin = (bearerHeader) => {
-  return validTokens.admin === getBearerToken(bearerHeader);
+  return validTokens.admin ? validTokens.admin === getBearerToken(bearerHeader) : false;
 }
 
 const isUser = (bearerHeader) => {
-  return validTokens.user === getBearerToken(bearerHeader);
+  return validTokens.user ? validTokens.user === getBearerToken(bearerHeader) : false;
 }
 
 module.exports = { getBearerToken, isAdmin, isUser };
